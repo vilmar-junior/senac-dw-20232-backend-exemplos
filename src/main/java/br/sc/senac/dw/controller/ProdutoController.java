190abfb3f960cd9b1d6bf9873068fb60d6fe505b
Vilmar César Pereira Júnior <vilmarcesarpereira@gmail.com>
2023-08-17 00:31:27 -0300

    Mais ajustes (ainda com problemas com o Lombok)

--- src/main/java/br/sc/senac/dw/controller/ProdutoController.java
+++ src/main/java/br/sc/senac/dw/controller/ProdutoController.java
@@ -1,5 +1,7 @@
 package br.sc.senac.dw.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,23 @@ public class ProdutoController {
 	
 	@GetMapping
 	public List<Produto> listarTodos() {
+		Produto p1 = new Produto(1, "Produto 1", "Fabricante 1", 10.0, 1.0, LocalDate.now());
+		Produto p2 = new Produto(2, "Produto 2", "Fabricante 2", 20.0, 2.0, LocalDate.now());
+		Produto p3 = new Produto(3, "Produto 3", "Fabricante 3", 30.0, 3.0, LocalDate.now());
+		
+		ArrayList<Produto> lista = new ArrayList<Produto>();
+		lista.add(p1);
+		lista.add(p2);
+		lista.add(p3);
+		lista.add(p3);
+		lista.add(p3);
+		lista.add(p3);
+		lista.add(p3);
+		lista.add(p3);
+		
+//		return lista;
+		
+		//TODO consultar no banco (através de um Repository/DAO)
 		return produtoServiceAntigoBO.listarTodos();
 	}
 }
--- src/main/java/br/sc/senac/dw/model/entidade/Produto.java
+++ src/main/java/br/sc/senac/dw/model/entidade/Produto.java
@@ -5,14 +5,15 @@ import java.time.LocalDate;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 //Anotação do lombok para gerar getters, setters e construtor vazio (não descobri ainda porque não gerou o construtor com todos os argumentos)
-@Getter
-@Setter
+//TODO ver esse artigo ("BUGFIX"): 
+//https://stackoverflow.com/questions/62168522/spring-boot-rest-api-returns-empty-json-used-with-lombok
+//@Data
 
 //Anotação do JPA para determinar que esta classe é uma entidade (objeto será gerenciado pelo container)
 @Entity
@@ -43,5 +44,53 @@ public class Produto {
 	
 	public Produto() {
 		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
 	}
 }
