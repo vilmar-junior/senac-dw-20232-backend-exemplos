3e57d5b8366df4347d5feb4d5f8ef14ec86a620c
Vilmar César Pereira Júnior <vilmarcesarpereira@gmail.com>
2023-08-21 18:47:00 -0300

    CRUD completo

--- src/main/java/br/sc/senac/dw/controller/ProdutoController.java
+++ src/main/java/br/sc/senac/dw/controller/ProdutoController.java
@@ -3,11 +3,16 @@ package br.sc.senac.dw.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.sc.senac.dw.exception.CampoInvalidoException;
 import br.sc.senac.dw.model.entidade.Produto;
 import br.sc.senac.dw.service.ProdutoService;
 
@@ -21,13 +26,60 @@ public class ProdutoController {
 	@Autowired //injeção de dependência (tomcat vai instanciar o objeto produtoService sob demanda)
 	private ProdutoService produtoService;
 	
+	/**
+	 * Método GET: geralmente utilizado em consultas
+	 * Parâmetros podem ser enviados via URL
+	 * 
+	 * @return a lista de todas os produtos
+	 */
 	@GetMapping(path = "/todos")
 	public List<Produto> listarTodos() {
 		return produtoService.listarTodos();
 	}
 	
-	@GetMapping(path = "/{id}")
-	public Produto consultarPorId(@PathVariable Integer id) {
+	/**
+	 * Método GET
+	 * Parâmetro id foi enviado via URL
+	 * @return um produto específico, dado o seu id
+	 */
+	@GetMapping("/detalhe/{id}")
+	public Produto pesquisarPorId(@PathVariable Integer id){
 		return produtoService.consultarPorId(id.longValue());
+	}
+	
+	/**
+	 * Método POST: geralmente utilizado para inserir novos registros
+	 * Parâmetros são enviados no corpo da requisição HTTP, 
+	 * para isso usamos a anotação @RequestBody
+	 * 
+	 * @return o produto salvo, com id preenchido
+	 * @throws CampoInvalidoException 
+	 */
+	@PostMapping
+	public Produto salvar(@RequestBody Produto novoProduto) throws CampoInvalidoException {
+		return produtoService.inserir(novoProduto);
+	}
+	
+	/**
+	 * Método PUT: utilizado para atualizar registros. 
+	 * Muitas vezes é usado o POST em seu lugar
+	 * Parâmetros são enviados no corpo da requisição HTTP, 
+	 * para isso usamos a anotação @RequestBody
+	 * 
+	 * @return um booleano indicando se a vacina em questão foi atualizada
+	 * @throws CampoInvalidoException 
+	 */
+	@PutMapping()
+	public boolean atualizar(@RequestBody Produto produtoParaAtualizar) throws CampoInvalidoException {
+		return produtoService.atualizar(produtoParaAtualizar) != null;
+	}
+	
+	/**
+	 * Método DELETE
+	 * Parâmetro id foi enviado via URL
+	 */
+	@DeleteMapping("/{id}")
+	public boolean excluir(@PathVariable Integer id) {
+		return produtoService.excluir(id);
 	}
 }
--- src/main/java/br/sc/senac/dw/service/ProdutoService.java
+++ src/main/java/br/sc/senac/dw/service/ProdutoService.java
@@ -1,12 +1,12 @@
 package br.sc.senac.dw.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.sc.senac.dw.exception.CampoInvalidoException;
 import br.sc.senac.dw.model.entidade.Produto;
 import br.sc.senac.dw.model.repository.ProdutoRepository;
 
@@ -26,5 +26,47 @@ public class ProdutoService {
 	public Produto consultarPorId(Long id) {
 		//fonte: https://stackoverflow.com/questions/52656517/no-serializer-found-for-class-org-hibernate-proxy-pojo-bytebuddy-bytebuddyinterc
 		return produtoRepository.findById(id.longValue()).get();
+	}
+
+	public Produto inserir(Produto novoProduto) throws CampoInvalidoException {
+		validarCamposObrigatorios(novoProduto);
+		return produtoRepository.save(novoProduto);
+	}
+	
+	public Produto atualizar(Produto produtoParaAtualizar) throws CampoInvalidoException {
+		validarCamposObrigatorios(produtoParaAtualizar);
+		return produtoRepository.save(produtoParaAtualizar);
+	}
+	
+	public boolean excluir(Integer id) {
+		produtoRepository.deleteById(id.longValue());
+		return true;
+	}
+	
+	//Métodos auxiliares
+	private void validarCamposObrigatorios(Produto produto) throws CampoInvalidoException {
+		String mensagemValidacao = "";
+		mensagemValidacao += validarCampoString(produto.getNome(), "nome");
+		mensagemValidacao += validarCampoString(produto.getFabricante(), "fabricante");
+		mensagemValidacao += validarCampoDouble(produto.getValor(), "valor");
+		mensagemValidacao += validarCampoDouble(produto.getPeso(), "peso");
+		
+		if(!mensagemValidacao.isEmpty()) {
+			throw new CampoInvalidoException(mensagemValidacao);
+		}
+	}
+
+	private String validarCampoString(String valorCampo, String nomeCampo) {
+		if(valorCampo == null || valorCampo.trim().isEmpty()) {
+			return "Informe o " + nomeCampo + " \n";
+		}
+		return "";
+	}
+
+	private String validarCampoDouble(Double valorCampo, String nomeCampo) {
+		if(valorCampo == null) {
+			return "Informe o " + nomeCampo + " \n";
+		}
+		return "";
 	}
 }
